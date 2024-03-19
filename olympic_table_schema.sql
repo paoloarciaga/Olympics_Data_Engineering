4b58823f402ea0a17176476b689087f7c15aae0b
larsenamy14 <larsenamy14@gmail.com>
2024-03-18 19:12:10 -0700

    Uploaded updated schema

--- olympic_table_schema.sql
+++ olympic_table_schema.sql
@@ -22,29 +22,10 @@ CREATE TABLE hosts (
 	end_date DATE,
 	season VARCHAR(6),
 	country VARCHAR(30),
-	city VARCHAR(30)
+	city VARCHAR(30),
+	game_year INTEGER
 	);
 
 SELECT * FROM hosts;
 
 DROP TABLE IF EXISTS results;
-
-CREATE TABLE results (
-	discipline_title VARCHAR(50),
-	event_title VARCHAR(300),
-	slug_game VARCHAR(30),
-	participant_type VARCHAR(10),
-	medal_type VARCHAR(6),
-	athletes VARCHAR(300),
-	rank_equal BOOLEAN,
-	rank_position VARCHAR(3),
-	country_name VARCHAR(50),
-	country_code VARCHAR(4),
-	country_3_letter_code VARCHAR(3),
-	athlete_url VARCHAR(100),
-	athlete_full_name VARCHAR(100),
-	value_unit VARCHAR(50),
-	value_type VARCHAR(50)
-	);
-
-SELECT * FROM results;
