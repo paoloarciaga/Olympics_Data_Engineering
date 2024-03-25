e148c5754229664c57d3d3230fe774ac04a153ba
larsenamy14 <larsenamy14@gmail.com>
2024-03-24 18:38:53 -0700

    Updated database query sql

--- Olympic_Files/olympic_database_query.sql
+++ Olympic_Files/olympic_database_query.sql
@@ -13,6 +13,10 @@ ORDER BY discipline_title ASC;
 SELECT COUNT (DISTINCT athlete_url)
 FROM athletes;
 
+--Count number of unique athletes in cleaned_results table
+SELECT COUNT (DISTINCT athlete_url)
+FROM cleaned_results;
+
 --Find all host countries included in hosts table
 SELECT DISTINCT(country)
 FROM hosts;
@@ -23,4 +27,18 @@ FROM medals AS m
 JOIN hosts AS h ON
 h.game_slug = m.slug_game
 WHERE h.country = 'Norway';
+
+--Medals won by individual athletes in ascending order
+SELECT a.first_name, a.last_name, cr.athlete_url, a.gold_medals
+FROM cleaned_results AS cr
+JOIN athletes AS a ON
+a.athlete_url = cr.athlete_url
+ORDER BY gold_medals DESC;
+
+--Count all gold medals, grouped by country in the London 2012 games
+SELECT COUNT(gold_medals), country_name, slug_game
+FROM cleaned_results
+WHERE slug_game = 'london-2012'
+GROUP BY country_name, slug_game
+ORDER BY COUNT(gold_medals) DESC;
 
